d9082f7251332270a8c9428f078e09fd1174d14f
mandaryevale <mandar788813@gmail.com>
2023-12-29 16:45:20 +0530

    first commit

---
+++ CMS/login.php
@@ -0,0 +1,226 @@
+<?php
+
+session_start();
+include 'config.php';
+// $host = "localhost";  
+// $username = "root";  
+// $password = "";  
+// $database = "db_bank";  
+$message = "";
+try {
+	//  $connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);  
+	// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+	if (isset($_POST["submit"])) {
+		if (empty($_POST["USER_NAME"]) || empty($_POST["USER_PSW"])) {
+			$message = '<label>All fields are required</label>';
+		} else {
+			$query = "SELECT * FROM user_master WHERE USER_NAME = :USER_NAME AND USER_PSW = :USER_PSW AND USER_STATUS='Enable'";
+			$statement = $conn->prepare($query);
+			$statement->execute(array('USER_NAME' => $_POST['USER_NAME'], 'USER_PSW' => $_POST['USER_PSW']));
+			$count = $statement->rowCount();
+
+			if ($count > 0) {
+				$_SESSION["USER_NAME"] = $_POST["USER_NAME"];
+				header("location:index2.php");
+			} else {
+				$message = '<label>Wrong Data</label>';
+			}
+		}
+	}
+} catch (PDOException $error) {
+	$message = $error->getMessage();
+}
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<!-- Basic Page Info -->
+	<meta charset="utf-8">
+	<title>CMS</title>
+
+	<!-- Site favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="vendors/images/apple-touch-icon.png">
+	<!-- <link rel="icon" type="image/png" sizes="32x32" href="vendors/images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="vendors/images/favicon-16x16.png"> -->
+
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="vendors/styles/core.css">
+	<link rel="stylesheet" type="text/css" href="vendors/styles/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="vendors/styles/style.css">
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-119386393-1');
+	</script>
+</head>
+
+<body class="login-page">
+	<!-- <div class="login-header box-shadow">
+		 <div class="container-fluid d-flex justify-content-between align-items-center">
+			<div class="brand-logo">
+				 <a href="login.html">
+					 <img src="vendors/images/deskapp-logo.svg" alt=""> 
+
+				</a> 
+
+
+			</div>
+			<div class="login-menu">
+				 <ul>
+					<li><a href="register.html">Register</a></li>
+				</ul> 
+			</div>
+		</div> 
+	</div> -->
+
+	<div class="login-wrap d-flex align-items-center flex-wrap justify-content-center">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-md-6 col-lg-7">
+					<!-- <img src="vendors/images/login-page-img.png" alt=""> -->
+				</div>
+				<div class="col-md-6 col-lg-5">
+					<div class="login-box bg-white box-shadow border-radius-10">
+						<div class="login-title">
+							<h1 class="text-center text-primary">CMS</h1>
+
+						</div>
+
+						<?php
+
+						if (isset($message)) {
+							echo '<label color="RED">' . $message . '</label>';
+						}
+						?>
+
+						<form method="POST">
+							<div class="select-role">
+								<div class="btn-group btn-group-toggle" data-toggle="buttons">
+									<!-- <label class="btn active">
+										<input type="radio" name="options" id="admin">
+										<div class="icon"><img src="vendors/images/briefcase.svg" class="svg" alt="">
+										</div>
+										<span>I'm</span>
+										Manager
+									</label>
+									<label class="btn">
+										<input type="radio" name="options" id="user">
+										<div class="icon"><img src="vendors/images/person.svg" class="svg" alt=""></div>
+										<span>I'm</span>
+										Employee
+									</label> -->
+								</div>
+							</div>
+
+
+							<div>
+
+								<label class="form-label">Username</label>
+
+							</div>
+							<div class="input-group custom">
+								<input type="text" class="form-control form-control-lg" placeholder="Username" name="USER_NAME">
+								<div class="input-group-append custom">
+									<span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
+								</div>
+							</div>
+
+
+							<div>
+								<label class="form-label">Password</label>
+
+							</div>
+
+
+							<div class="input-group custom">
+								<input type="password" class="form-control form-control-lg" placeholder="**********" name="USER_PSW">
+								<div class="input-group-append custom">
+									<span class="input-group-text"><i class="dw dw-padlock1"></i></span>
+								</div>
+							</div>
+							<div class="row pb-30">
+								<!-- <div class="col-6">
+									<div class="custom-control custom-checkbox">
+										<input type="checkbox" class="custom-control-input" id="customCheck1">
+										<label class="custom-control-label" for="customCheck1">Remember</label>
+									</div>
+								</div>
+								<div class="col-6">
+									<div class="forgot-password"><a href="forgot-password.html">Forgot Password</a>
+									</div>
+								</div> -->
+							</div>
+							<div class="row">
+								<div class="col-sm-12">
+									<div class="input-group mb-0">
+
+
+										<input class="btn btn-primary btn-lg btn-block" type="submit" name="submit" value="Sign In">
+
+
+
+										<!-- <a class="btn btn-primary btn-lg btn-block" href="index.html">Sign In</a> -->
+									</div>
+
+									<span></span>
+									<!-- <div class="font-16 weight-600 pt-10 pb-10 text-center" data-color="#707373">OR
+									</div>
+									<div class="input-group mb-0">
+										<a class="btn btn-outline-primary btn-lg btn-block"
+											href="register.html">Register To Create Account</a>
+									</div> -->
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- js -->
+	<script src="vendors/scripts/core.js"></script>
+	<script src="vendors/scripts/script.min.js"></script>
+	<script src="vendors/scripts/process.js"></script>
+	<script src="vendors/scripts/layout-settings.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
